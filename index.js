5250b2dbc89502020d9ae023c09918740a8d0c7f
Nathanael <nblack@salesforce.com>
2016-07-05 10:39:09 -0700

    add deploy code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const archiver = require('archiver');
 const fs = require('fs');
 const path = require('path');
 
+const jsforceConnection = require('jsforce-connection');
 
 const zip = function(dir, outDir='.') {
       const archive = archiver('zip');
@@ -24,11 +25,49 @@ const zip = function(dir, outDir='.') {
       return outFile;
 };
 
+const info = function(message) {
+    console.log(`       ${message}`);
+};
+
+const error = function(message) {
+    console.error(` !     ${message}`);
+};
+
 const deploy = function(conn) {
     return function(file) {
         console.log(`Packaging ${file}`);
-        const archive = zip(file);
-        console.log(`Deploying ${archive}`);
+        const zipPath = zip(file);
+        console.log(`Deploying ${zipPath}`);
+
+        return jsforceConnection()
+            .then( conn => {
+                return new Promise((resolve, reject) => {
+                    console.log('-----> Deploying metadata');
+                    const zipStream = fs.createReadStream(zipPath);
+                    conn.metadata.pollTimeout = 240*1000;
+                    const deployLocator = conn.metadata.deploy(zipStream, {});
+                    deployLocator.complete(true, function(err, result) {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                        resolve(result);
+                    });
+                })
+            })
+            .then( result => {
+                info('done ? :' + result.done);
+                info('success ? : ' + result.true);
+                info('state : ' + result.state);
+                info('component errors: ' + result.numberComponentErrors);
+                info('components deployed: ' + result.numberComponentsDeployed);
+                info('tests completed: ' + result.numberTestsCompleted);
+                info('       ' + (result.success ? 'Success' : 'Failed'));
+            })
+            .catch( err => {
+                error(err.stack);
+                process.exit(1);
+            });
     }
 };
 
